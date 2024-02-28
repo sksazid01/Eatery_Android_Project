af647e5599b799d5662a9b79a19c1702335b5fd9
sksazid01 <1.sksazid@gmail.com>
2024-02-28 19:13:01 +0600

    After adding database

--- app/src/main/java/com/example/eateryapp/Screens/RestaurantClass.kt
+++ app/src/main/java/com/example/eateryapp/Screens/RestaurantClass.kt
@@ -170,7 +170,7 @@ fun RestaurantCard(resName:String,status:Boolean,navController: NavController,in
         signalColor= Color.White
         openOrclose = "Open"
     }
-
+//
     else {
         signalColor= Color.Red
         openOrclose = "Close"
